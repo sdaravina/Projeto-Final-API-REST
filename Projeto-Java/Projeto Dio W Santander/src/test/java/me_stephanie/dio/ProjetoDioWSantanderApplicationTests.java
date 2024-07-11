294362236f749df8dfc2d2e5ced9c7ec12433582
Stephanie Daravina <sdaravina97@gmail.com>
2024-07-11 01:52:16 -0300

    Projeto ok

---
+++ Projeto-Java/Projeto Dio W Santander/src/test/java/me_stephanie/dio/ProjetoDioWSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package me_stephanie.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioWSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Projeto-Java/README.md
@@ -0,0 +1,6 @@
+# Projeto Final Bootcamp DIO + Santander 
+
+### Objetivo deste projeto é a prática dos conhecimentos adquiridos em aulas. 
+
+Para esta prática, foi útilizado a IDE VSCODE. 
+
